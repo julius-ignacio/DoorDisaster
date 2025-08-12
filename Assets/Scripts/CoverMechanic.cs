abf4a56287ada29b59e5df761e742d13f394b4ad
Julius <ignacio221julius@gmail.com>
2025-08-13 01:57:49 +0800

    partially added cover mechanic

--- Assets/Scripts/CoverMechanic.cs
+++ Assets/Scripts/CoverMechanic.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class CoverMechanic : MonoBehaviour
 {
 
-    public GameObject Player;
+    public GameObject Player, Model;
     public Camera CoverCamera;
 
+    void Start()
+    {
+        Player.SetActive(true); // Ensure player is active at start
+        Model.SetActive(false); // Ensure cover model is inactive at start
+        CoverCamera.gameObject.SetActive(false); // Ensure cover camera is inactive at start
+    }
 
- public void OnButtonClick()
+
+    public void OnButtonClick()
     {
         Debug.Log("Button was clicked!");
         Player.SetActive(false); // Hide the player
+        Model.SetActive(true); // Show the cover model
         CoverCamera.gameObject.SetActive(true); // Activate the cover camera
     }
 }
