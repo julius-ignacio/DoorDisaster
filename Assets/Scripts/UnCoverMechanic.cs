14bc6a312d48e924a4c81c19bc4e65599b1c405c
Julius <ignacio221julius@gmail.com>
2025-08-14 20:45:11 +0800

    joystick works, cover mechanic works

--- Assets/Scripts/UnCoverMechanic.cs
+++ Assets/Scripts/UnCoverMechanic.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class UnCoverMechanic : MonoBehaviour
 {
 
-    public GameObject Player;
+    public GameObject Player, Model, CoverBtn, UnCoverBtn, joystick, jumpbtn;
     public Camera CoverCamera;
 
 
- public void OnButtonClick()
+    public void OnButtonClick()
     {
         Debug.Log("Button was clicked!");
         Player.SetActive(true); // Unhide the player
+        Model.SetActive(false); // Show the cover model
         CoverCamera.gameObject.SetActive(false); // Activate the cover camera
+
+        CoverBtn.SetActive(true); // Hide the button after clicking
+        UnCoverBtn.SetActive(false); // Hide the button after clicking
+        
+        joystick.SetActive(true); // Show the joystick
+        jumpbtn.SetActive(true); // Show the jump button
     }
 }
