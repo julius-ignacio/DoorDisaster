9a20eb751fdeaf9e70117857f2c9dcc076c5ec6f
Julius <ignacio221julius@gmail.com>
2025-08-25 01:55:02 +0800

    added heartbeat sfx, gameover screen, consistentEQ, and more

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,17 +2,30 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    public GameObject Joystick, Jumpbtn, Player, GameOverUI, PanicMeterUI, CoverBtn, uncoverBtm;
+    public GameObject Joystick, Jumpbtn, GameOverUI, PanicMeterUI, CoverBtn, uncoverBtm;
+    public PanicMeterScript panicMeterScript;
+    public Movements movementscript;
 
 
-public void playerGameOver()
+
+    public void playerGameOver()
     {
         Joystick.SetActive(false);
         Jumpbtn.SetActive(false);
-        Player.SetActive(false);
         GameOverUI.SetActive(true);
         PanicMeterUI.SetActive(false);
         CoverBtn.SetActive(false);
         uncoverBtm.SetActive(false);
+
+        movementscript.enabled = false;
+    }
+
+
+    void Update()
+    {
+        if (panicMeterScript.currHealth >= 100)
+        {
+            playerGameOver();
+        }
     }
 }
