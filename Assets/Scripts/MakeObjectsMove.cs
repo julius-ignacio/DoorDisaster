36a1cef23dd0021b826341c16dbcc6d22a66904e
Julius <ignacio221julius@gmail.com>
2025-08-29 02:23:30 +0800

    added locker falling mechanics

--- Assets/Scripts/MakeObjectsMove.cs
+++ Assets/Scripts/MakeObjectsMove.cs
@@ -6,6 +6,7 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class MakeObjectsMove : MonoBehaviour
 {
+    public GameObject[] locker;
 
     public float chairMoveSpeed = 1f;
     private AudioSource audi;
@@ -98,14 +99,69 @@ public class MakeObjectsMove : MonoBehaviour
 
         //     }
         // }
-        
+
+
+
+
+        switch (collision.gameObject.name)
+        {
+            case "hallway1":
+                { locker[0].GetComponent<LockerFall>().Fall(); }
+                break;
+
+            case "hallway2":
+                { locker[1].GetComponent<LockerFall2>().Fall(); }
+                break;
+
+            case "hallway3":
+                {
+                    locker[2].GetComponent<LockerFall>().Fall();
+                    locker[3].GetComponent<LockerFall>().Fall();
+                    locker[4].GetComponent<LockerFall>().Fall();
+                    locker[5].GetComponent<LockerFall2>().Fall();
+                }
+                break;
+
+            case "hallway4":
+                {
+                    locker[6].GetComponent<LockerFall2>().Fall();
+                    locker[7].GetComponent<LockerFall2>().Fall();
+                    locker[8].GetComponent<LockerFall2>().Fall();
+                }
+                break;
+
+            case "hallway5":
+                {
+                    locker[9].GetComponent<LockerFall2>().Fall();
+                    locker[10].GetComponent<LockerFall2>().Fall();
+                    locker[11].GetComponent<LockerFall2>().Fall();
+                    locker[12].GetComponent<LockerFall2>().Fall();
+                    locker[13].GetComponent<LockerFall2>().Fall();
+                }
+                break;
+
+
+            case "hallway6":
+                {
+                    locker[14].GetComponent<LockerFall2>().Fall();
+                    locker[15].GetComponent<LockerFall2>().Fall();
+                    locker[16].GetComponent<LockerFall>().Fall();
+                    locker[17].GetComponent<LockerFall>().Fall();
+                    locker[18].GetComponent<LockerFall>().Fall();
+                }
+                break;
+
+        }
+
+
+
 
 
     }
-    
 
 
-    
+
+
 
 
 
