beaf209ddcdc5c6f442667e68558e3f4b531de31
Julius <ignacio221julius@gmail.com>
2025-08-29 02:23:30 +0800

    added locker falling mechanics

--- Assets/Scripts/LockerFall2.cs
+++ Assets/Scripts/LockerFall2.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LockerFall : MonoBehaviour
+public class LockerFall2 : MonoBehaviour
 {
     public Rigidbody rb;
 
@@ -12,6 +12,6 @@ public class LockerFall : MonoBehaviour
     public void Fall()
     {
         rb.isKinematic = false; // Enable physics
-        rb.AddTorque(Vector3.forward * 200f); // Push it over
+        rb.AddTorque(Vector3.back * 200f); // Push it over
     }
 }
