ccc80acaa2c4284cc03b835c39a8a74e3e6342cc
Julius <ignacio221julius@gmail.com>
2025-09-19 07:47:40 +0800

    facts system now worksgit add .!

--- Assets/GiveFactIndex.cs
+++ Assets/GiveFactIndex.cs
@@ -1,20 +1,20 @@
-// using UnityEngine;
+using UnityEngine;
 
-// public class GiveFactIndex : MonoBehaviour
-// {
-//     public discoverFacts _discoverfacts;
-//     public int indexForFacts;
-//     void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             _discoverfacts.factIndex = indexForFacts;
-//             _discoverfacts.ReadBtn.SetActive(true);
-//         }
-//     }
+public class GiveFactIndex : MonoBehaviour
+{
+    public discoverFacts _discoverfacts;
+    public int indexForFacts;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _discoverfacts.factIndex = indexForFacts;
+            _discoverfacts.ReadBtn.SetActive(true);
+        }
+    }
 
-//      void OnTriggerExit(Collider other)
-//     {
-//         if (other.CompareTag("Player")) { _discoverfacts.ReadBtn.SetActive(false); _discoverfacts.facts[_discoverfacts.factIndex].SetActive(false); }
-//     }
-// }
+     void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) { _discoverfacts.ReadBtn.SetActive(false); _discoverfacts.facts[_discoverfacts.factIndex].SetActive(false); }
+    }
+}
