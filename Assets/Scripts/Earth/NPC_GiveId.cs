6a76e49ac518d567e9d5b6a43a97499a56bab4ea
Julius <ignacio221julius@gmail.com>
2025-09-19 13:10:04 +0800

    facts works

---
+++ Assets/Scripts/Earth/NPC_GiveId.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class NPC_GiveId : MonoBehaviour
+{
+    // public int NpcId;
+    // public QuizScript quizScript;
+
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     { 
+    //         quizScript.currentNpcId = NpcId; // Pass the NPC ID to QuizScript
+    //      }
+    // }
+}
