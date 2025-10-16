fe183375242b21e43245ad549461cb645b91fd4b
Julius C. Ignacio <ignacio221julius@gmail.com>
2025-10-16 13:04:42 +0800

    disaster update works now. refactored game trial start

--- Assets/Scripts/GameMenu_switchers.cs
+++ Assets/Scripts/GameMenu_switchers.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class GameMenu_switchers : MonoBehaviour
 {
-    public GameObject mainMenu, informationMenu;
-    public GameObject disasterUpdates, hotlines;
+    public GameObject mainMenu;
+    public RectTransform informationMenu; // ✅ use RectTransform instead of GameObject
+    public GameObject disasterUpdates;
+    public GameObject hotlines;
+    public Eq_info_loader loader;
+
+    private Vector2 hiddenPosition = new Vector2(0, 2224);
+    private Vector2 visiblePosition = new Vector2(0, 0);
 
     void Start()
     {
-        informationMenu.SetActive(false);
+        // Move InformationMenu off-screen at start
+        informationMenu.anchoredPosition = hiddenPosition;
+
         hotlines.SetActive(false);
         disasterUpdates.SetActive(false);
         mainMenu.SetActive(true);
@@ -15,15 +23,33 @@ public class GameMenu_switchers : MonoBehaviour
 
     public void OpenInformationMenu()
     {
-        informationMenu.SetActive(true);
+        informationMenu.anchoredPosition = visiblePosition;
+
         disasterUpdates.SetActive(true);
         mainMenu.SetActive(false);
+
+        // ✅ Trigger data loading when user opens the menu
+        if (loader != null)
+            StartCoroutine(loader.LoadEarthquakeData());
+    }
+
+    public void CloseInformationMenu()
+    {
+        informationMenu.anchoredPosition = hiddenPosition;
+
+        disasterUpdates.SetActive(false);
+        hotlines.SetActive(false);
+        mainMenu.SetActive(true);
     }
 
     public void OpenDisasterUpdates()
     {
         disasterUpdates.SetActive(true);
         hotlines.SetActive(false);
+
+        // Optionally reload data each time
+        if (loader != null)
+            StartCoroutine(loader.LoadEarthquakeData());
     }
 
     public void OpenHotlines()
